54b1339a9fe29225e581b9ae3e35b6b10084f015
Levis Artur <120251909+Levis-Artur@users.noreply.github.com>
2023-08-29 20:08:57 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+import { Container, Title, SubTitle } from './App.styled';
+import { nanoid } from 'nanoid';
+
+const useLocalStorage = (key, defValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, window.JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+};
+
+const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useLocalStorage('filter', '');
+
+  const saveData = data => {
+    let isDuplicate = contacts.find(elem => {
+      return elem.name.toLowerCase() === data.name.toLowerCase();
+    });
+    if (isDuplicate) {
+      alert(`${data.name} is alredy in contacts!`);
+      return true;
+    }
+    setContacts(prevState => {
+      return [...prevState, { id: nanoid(), ...data }];
+    });
+  };
+
+  const setFilters = e => {
+    const { value } = e.target;
+    setFilter(value);
+  };
+
+  const handleDelete = id => {
+    setContacts(prevState => {
+      return prevState.filter(item => item.id !== id);
+    });
+  };
+  const getVisibleContacts = () => {
+    let colectionForRender = contacts.filter(item => {
+      return item.name
+        .trim()
+        .toLocaleLowerCase()
+        .includes(filter.trim().toLocaleLowerCase());
+    });
+
+    return colectionForRender;
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm saveData={saveData} />
+
+      <SubTitle>Contacts</SubTitle>
+      <Filter filterText={filter} onFilter={setFilters} />
+      <ContactList contacts={getVisibleContacts()} onDelete={handleDelete} />
+    </Container>
   );
 };
+
+export { App };
